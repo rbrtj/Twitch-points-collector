d55dc49d1dad8742a7743c3ca600c607a041a263
rbrtj <robertjaszczurek@icloud.com>
2024-08-21 19:30:15 +0200

    Collect points

---
+++ index.js
@@ -0,0 +1,16 @@
+const isTwitchUrl = window.location.href.includes("twitch.tv");
+
+function collectPoints() {
+  if (isTwitchUrl) {
+    console.log("Running!");
+    const element = document.querySelector('[aria-label="Claim Bonus"]');
+    if (element) {
+      element.click();
+    }
+  }
+}
+
+new MutationObserver(collectPoints).observe(document.body, {
+  childList: true,
+  subtree: true,
+});
